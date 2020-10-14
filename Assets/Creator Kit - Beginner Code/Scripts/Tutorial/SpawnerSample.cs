2c59c348f8dcbc2a61e1035ec3ad95e2bf5f9989
solidogen <solidogen@gmail.com>
2020-10-14 21:51:30 +0200

    LootAngle helper

--- Assets/Creator Kit - Beginner Code/Scripts/Tutorial/SpawnerSample.cs
+++ Assets/Creator Kit - Beginner Code/Scripts/Tutorial/SpawnerSample.cs
@@ -7,10 +7,12 @@ public class SpawnerSample : MonoBehaviour
 
     void Start()
     {
-        SpawnObject(angle: 0, radius: 3);
-        SpawnObject(angle: 45, radius: 4);
-        SpawnObject(angle: 90, radius: 5);
-        SpawnObject(angle: 180, radius: 6);
+        LootAngle lootAngle = new LootAngle(45);
+
+        SpawnObject(angle: lootAngle.NextAngle(), radius: 3);
+        SpawnObject(angle: lootAngle.NextAngle(), radius: 4);
+        SpawnObject(angle: lootAngle.NextAngle(), radius: 5);
+        SpawnObject(angle: lootAngle.NextAngle(), radius: 6);
     }
 
     void SpawnObject(int angle, int radius)
@@ -21,3 +23,20 @@ public class SpawnerSample : MonoBehaviour
     }
 }
 
+public class LootAngle {
+    int angle;
+    int step;
+
+    public LootAngle(int increment) {
+        step = increment;
+        angle = 0;
+    }
+    
+    public int NextAngle()
+    {
+        int currentAngle = angle;
+        angle = Helpers.WrapAngle(angle + step);
+        
+        return currentAngle;
+    }
+}
